59aab0121afb7ac111b8362efbbce192eb264e54
Abdulrahman <abdulrahman650>
2025-12-18 21:24:43 +0200

    Add Feature ListViewItem to home

--- lib/features/home/view/widgets/custom_ListViewItem.dart
+++ lib/features/home/view/widgets/custom_ListViewItem.dart
@@ -6,17 +6,14 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height *.3,
-      child: AspectRatio(
-        aspectRatio: 2.7/4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius:BorderRadius.circular(16),
-              color: Colors.white,
-              image: DecorationImage(image:AssetImage( Assets.imagesTestImage,
-              ),fit: BoxFit.fill)
-          ),
+    return AspectRatio(
+      aspectRatio: 2.7/4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius:BorderRadius.circular(16),
+            color: Colors.white,
+            image: DecorationImage(image:AssetImage( Assets.imagesTestImage,
+            ),fit: BoxFit.fill)
         ),
       ),
     );
