9276879da0f25d041698108d278f1445ce323279
Abdulrahman <abdulrahman650>
2025-12-20 20:49:34 +0200

    add custom button

---
+++ lib/core/utils/custom_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.backgroundColor,
+    required this.textColor,
+    this.borderRadius,
+    required this.textt,
+    this.fontSize,
+    this.onPressed,
+  });
+  final String textt;
+  final Color backgroundColor;
+  final Color textColor;
+  final BorderRadius? borderRadius;
+  final double? fontSize;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    final text = Theme.of(context).textTheme;
+
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ??
+                BorderRadius.circular(
+                  12,
+                ),
+          ),
+        ),
+        child: Text(
+          textt,
+          style: text.titleMedium!.copyWith(
+            color: textColor,
+            fontWeight: FontWeight.w900,
+            fontSize: fontSize,
+          ),
+        ),
+      ),
+    );
+  }
+}
