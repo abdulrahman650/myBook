ed84e88065ea2fe5c5a4b11574c41b0e9978c91b
Abdulrahman <abdulrahman650>
2025-12-20 20:55:07 +0200

    add Similar Books Section

---
+++ lib/features/books_details/view/widgets_books_details/similar_books_section.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mybook/features/books_details/view/widgets_books_details/similar_books_list_view.dart';
+
+
+class SimilarBooksSection extends StatelessWidget {
+  const SimilarBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final text = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'You can also like',
+          style: text.titleSmall!.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const SimilarBooksListview(),
+      ],
+    );
+  }
+}
