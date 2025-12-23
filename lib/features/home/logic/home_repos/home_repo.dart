cd1e405c0c54964d1dfec41a361d451babf2fdd7
Abdulrahman <abdulrahman650>
2025-12-23 16:50:52 +0200

    Create HomeRepo Methods and failure file

--- lib/features/home/logic/home_repos/home_repo.dart
+++ lib/features/home/logic/home_repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:mybook/features/home/data/book_model.dart';
+
+import '../../../../core/erorrs/failures.dart';
+
 abstract class HomeRepo{
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+ Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
