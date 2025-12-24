f72cc18a08ee278f55e798a03706c2d9f86612ed
Abdulrahman <abdulrahman650>
2025-12-24 19:57:45 +0200

    setup Service Locator

---
+++ lib/core/services/services_locator.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mybook/core/services/api_services.dart';
+import 'package:mybook/features/home/logic/home_repos/home_repo.dart';
+import 'package:mybook/features/home/logic/home_repos/home_repo_impl.dart';
+//بعمل انشاء لسيرفيس مره واحده وبعدين بعيد استخدامها بدل ما كا شويه اكتبها
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+    getIt.get<ApiService>(),
+  ));
+}
