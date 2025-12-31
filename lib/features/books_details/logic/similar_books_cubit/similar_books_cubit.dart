a9e5acac70ef03fe0886e831a9f80ef7a50b9ce3
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2025-12-31 16:19:59 +0200

    add similar books cubit& state

---
+++ lib/features/books_details/logic/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../home/data/book_model/book_model.dart';
+import '../../../home/logic/home_repos/home_repo.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilarBooks({required String category}) async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchSimilarBooks(category: category);
+
+    result.fold(
+      (Failure) {
+        emit(SimilarBooksFailure(Failure.errMessage));
+      },
+
+      (books) {
+        emit(SimilarBooksSuccess(books));
+      },
+    );
+  }
+}
