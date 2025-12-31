e52058b56bf79f0b42037d4d9047adb4c8f89172
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2025-12-31 16:32:57 +0200

    edit errors

--- lib/features/home/view/widgets/feature_listview.dart
+++ lib/features/home/view/widgets/feature_listview.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:mybook/core/utils/custom_error_widget.dart';
-import 'package:mybook/core/utils/custom_loading_indicator.dart';
 import 'package:mybook/features/books_details/view/widgets_books_details/books_datiles_view_body.dart';
 import 'package:mybook/features/home/logic/featured_books_cubit/featured_books_cubit.dart';
-
-import '../../../../core/utils/app_router.dart';
+import '../../../../core/utiles/app_router.dart';
+import '../../../../core/utiles/custom_error_widget.dart';
+import '../../../../core/utiles/custom_loading_indicator.dart';
 import '../../../books_details/view/book_details_view.dart';
 import 'custom_book_item.dart';
 
@@ -17,37 +16,38 @@ class FeaturedBooksListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
       builder: (context, state) {
-        if(state is FeaturedBooksSuccess){
+        if (state is FeaturedBooksSuccess) {
           return SizedBox(
-            height: MediaQuery
-                .of(context)
-                .size
-                .height * .3,
+            height: MediaQuery.of(context).size.height * .3,
             child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: state.books.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: GestureDetector(
-                        onTap: () {
-                          GoRouter.of(context).push(AppRouter.bookDetailsView);
-                        },
-                        child: CustomBookImage(
-                          imageUrl: state.books[index].volumeInfo.imageLinks!.thumbnail
-                        )),
-                  );
-                }),
+              physics: const BouncingScrollPhysics(),
+              itemCount: state.books.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(
+                        AppRouter.bookDetailsView,
+                        extra: state.books[index],
+                      );
+                    },
+                    child: CustomBookImage(
+                      imageUrl:
+                      state.books[index].volumeInfo.imageLinks?.thumbnail ??
+                          '',
+                    ),
+                  ),
+                );
+              },
+            ),
           );
-        }else if (state is FeaturedBooksFailure){
-return CustomErrorWidget(errMessage:state.errMessage);
-
-        }else{
-          return CustomLoadingIndicator();
-
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorsWidget(errorMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
         }
-
       },
     );
   }
