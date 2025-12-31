ff6609bc7667ccf84b89d3163378cd91b4e32130
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-01-01 00:30:57 +0200

    handling SearchResultListView & add blocBuilder

--- lib/features/search/view/widgets_search_view/search_result_listview.dart
+++ lib/features/search/view/widgets_search_view/search_result_listview.dart
@@ -1,23 +1,47 @@
-// import 'package:flutter/material.dart';
-//
-// import '../../../home/view/widgets/best_seller_listView_item.dart';
-// class SearchResultListView extends StatelessWidget {
-//   const SearchResultListView({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       padding: EdgeInsets.zero,
-//       itemCount: 10,
-//       itemBuilder: (context, index) {
-//         return const Padding(
-//           padding: EdgeInsets.symmetric(vertical: 10),
-//           child: BookListViewItem(
-//             bookModel: null,
-//           ),
-//           // child: Text('data'),
-//         );
-//       },
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/utiles/custom_error_widget.dart';
+import '../../../home/view/widgets/best_seller_listView_item.dart';
+import '../../logic/search_cubit/search_cubit.dart';
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        if (state is SearchLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is SearchFailure) {
+          return CustomErrorsWidget(errorMessage: state.errMessage);
+        } else if (state is SearchSuccess) {
+          return ListView.builder(
+            itemCount: state.books.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: BookListViewItem(bookModel: state.books[index]),
+              );
+            },
+          );
+        } else {
+          return const Center(child: Text('Search for any Book'));
+        }
+      },
+    );
+
+    // return ListView.builder(
+    //   padding: EdgeInsets.zero,
+    //   itemCount: 10,
+    //   itemBuilder: (context, index) {
+    //     return Padding(
+    //       padding: const EdgeInsets.symmetric(vertical: 10.0),
+    //       // child: const BookListViewItem(),
+    //       child: Text('data'),
+    //     );
+    //   },
+    // );
+  }
+}
+
