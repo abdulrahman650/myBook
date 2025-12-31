6423875214cc2284b4b7e9f1cf6d461d2ac05ba3
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2025-12-31 15:56:51 +0200

    FeaturedBooks cubit & state

--- lib/features/home/logic/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/logic/featured_books_cubit/featured_books_cubit.dart
@@ -1,23 +1,68 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-import 'package:mybook/features/home/data/book_model.dart';
+import 'package:equatable/equatable.dart';
 import 'package:mybook/features/home/logic/home_repos/home_repo.dart';
 
 import '../../data/book_model/book_model.dart';
 
 part 'featured_books_state.dart';
+//
+// class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+//   FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+//   final HomeRepo homeRepo;
+//   Future<void> fetchFeaturedBooks()async{
+//     emit(FeaturedBooksLoading());
+//     final result = await homeRepo.fetchFeaturedBooks();
+//     result.fold((failure)
+//     {
+//       emit(FeaturedBooksFailure(failure.errMessage));
+//     }, (books)
+//     {
+//       emit(FeaturedBooksSuccess(books));
+//     });
+//   }
+// }
+
+
+
+
+// class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+//   final HomeRepo homeRepo;
+//
+//   FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+//
+//   Future<void> fetchFeaturedBooks() async {
+//     emit(FeaturedBooksLoading());
+//
+//     try {
+//       final result = await homeRepo.fetchFeaturedBooks();
+//
+//       result.fold(
+//             (failure) {
+//           emit(FeaturedBooksFailure(failure.errMessage));
+//         },
+//             (books) {
+//           // لو القائمة فاضية، يظهر رسالة في الـ Widget
+//           emit(FeaturedBooksSuccess(books));
+//         },
+//       );
+//     } catch (e) {
+//       emit(FeaturedBooksFailure(e.toString()));
+//     }
+//   }
+// }
+
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
   FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
   final HomeRepo homeRepo;
-  Future<void> fetchFeaturedBooks()async{
+
+  Future<void> fetchFeaturedBooks() async {
     emit(FeaturedBooksLoading());
-    final result = await homeRepo.fetchFeaturedBooks();
-    result.fold((failure)
-    {
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
       emit(FeaturedBooksFailure(failure.errMessage));
-    }, (books)
-    {
+    }, (books) {
       emit(FeaturedBooksSuccess(books));
     });
   }
--- lib/features/home/logic/featured_books_cubit/featured_books_state.dart
+++ lib/features/home/logic/featured_books_cubit/featured_books_state.dart
@@ -1,16 +1,58 @@
+// part of 'featured_books_cubit.dart';
+//
+// @immutable
+// sealed class FeaturedBooksState {}
+//
+// final class FeaturedBooksInitial extends FeaturedBooksState {}
+// final class FeaturedBooksLoading extends FeaturedBooksState {}
+// final class FeaturedBooksFailure extends FeaturedBooksState {
+//   final String errMessage;
+//   FeaturedBooksFailure(this.errMessage);
+//
+// }
+// final class FeaturedBooksSuccess extends FeaturedBooksState {
+//   final List<BookModel> books;
+//   FeaturedBooksSuccess(this.books);
+// }
+// part of 'featured_books_cubit.dart';
+//
+// @immutable
+// sealed class FeaturedBooksState {}
+//
+// final class FeaturedBooksInitial extends FeaturedBooksState {}
+//
+// final class FeaturedBooksLoading extends FeaturedBooksState {}
+//
+// final class FeaturedBooksFailure extends FeaturedBooksState {
+//   final String errMessage;
+//   FeaturedBooksFailure(this.errMessage);
+// }
+//
+// final class FeaturedBooksSuccess extends FeaturedBooksState {
+//   final List<BookModel> books;
+//   FeaturedBooksSuccess(this.books);
+// }
 part of 'featured_books_cubit.dart';
 
-@immutable
-sealed class FeaturedBooksState {}
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
 
-final class FeaturedBooksInitial extends FeaturedBooksState {}
-final class FeaturedBooksLoading extends FeaturedBooksState {}
-final class FeaturedBooksFailure extends FeaturedBooksState {
-  final String errMessage;
-  FeaturedBooksFailure(this.errMessage);
-
+  @override
+  List<Object> get props => [];
 }
-final class FeaturedBooksSuccess extends FeaturedBooksState {
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  const FeaturedBooksFailure(this.errMessage);
+}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel> books;
-  FeaturedBooksSuccess(this.books);
+
+  const FeaturedBooksSuccess(this.books);
 }
