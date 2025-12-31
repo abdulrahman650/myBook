d261f434253e3f3f9eb4961dc33f99c7e7e1909d
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-01-01 00:32:39 +0200

    handling & add blocBuilder

--- lib/features/search/view/search_view.dart
+++ lib/features/search/view/search_view.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mybook/features/search/view/widgets_search_view/search_view_body.dart';
+
+import '../../../core/services/api_services.dart';
+import '../../../core/services/services_locator.dart';
+import '../logic/repos/search_repo_impl.dart';
+import '../logic/search_cubit/search_cubit.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -7,16 +13,13 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("fdgfdg")
-        ],
-      )
-      // SafeArea(
-      //     child:
-      //     // SearchViewBody(),
-      //
-      // ),
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) =>
+              SearchCubit(SearchRepoImpl(getIt.get<ApiService>())),
+          child: const SearchViewBody(),
+        ),
+      ),
     );
 
 
--- lib/features/search/view/widgets_search_view/search_view_body.dart
+++ lib/features/search/view/widgets_search_view/search_view_body.dart
@@ -1,32 +1,32 @@
-// import 'package:flutter/material.dart';
-// import 'package:gap/gap.dart';
-// import 'package:mybook/features/search/view/widgets_search_view/search_result_listview.dart';
-//
-// import 'custom_search_text_field.dart';
-// class SearchViewBody extends StatelessWidget {
-//   const SearchViewBody({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final text = Theme.of(context).textTheme;
-//
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 30),
-//       child: Column(
-//         children: [
-//           CustomSearchTextField(),
-//           Gap(16),
-//           Text(
-//             'Search Result',
-//             style: text.titleMedium,
-//           ),
-//           Gap(16),
-//           Expanded(
-//             child: SearchResultListView(),
-//           ),
-//         ],
-//       ),SearchResultListView
-//     );
-//   }
-// }
-//
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:mybook/features/search/view/widgets_search_view/search_result_listview.dart';
+
+import 'custom_search_text_field.dart';
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final text = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+          Gap(16),
+          Text(
+            'Search Result',
+            style: text.titleMedium,
+          ),
+          Gap(16),
+          Expanded(
+            child: SearchResultListView(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
