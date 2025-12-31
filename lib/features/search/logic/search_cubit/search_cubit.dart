ff559ccc8a6504c8fff3665079c8439791329cab
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-01-01 00:24:36 +0200

    create Search cubit& state

---
+++ lib/features/search/logic/search_cubit/search_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../home/data/book_model/book_model.dart';
+import '../repos/search_repo.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  final SearchRepo searchRepo;
+  SearchCubit(this.searchRepo) : super(SearchInitial());
+
+  void SearchBooks({required String query}) async {
+    if (query.isEmpty) return;
+
+    emit(SearchLoading());
+
+    var result = await searchRepo.searchBooks(query: query);
+
+    result.fold(
+      (failure) => emit(SearchFailure(failure.errMessage)),
+      (books) => emit(SearchSuccess(books)),
+    );
+  }
+}
