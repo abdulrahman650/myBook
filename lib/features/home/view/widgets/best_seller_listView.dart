3541ce325f33217a389d63c6ed02daf8457ad6cb
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2025-12-31 16:32:14 +0200

    add book model and data

--- lib/features/home/view/widgets/best_seller_listView.dart
+++ lib/features/home/view/widgets/best_seller_listView.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/utiles/custom_error_widget.dart';
+import '../../../../core/utiles/custom_loading_indicator.dart';
+import '../../logic/newest_books_cubit/newest_books_cubit.dart';
 import 'best_seller_listView_item.dart';
 class BestSellerListview extends StatelessWidget {
   const BestSellerListview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        padding: EdgeInsets.zero,
-        itemCount: 10,
-        itemBuilder: (context,index){
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: BookListViewItem(),
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        if (state is NewestBooksSuccess) {
+          return ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemCount: state.books.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: BookListViewItem(bookModel: state.books[index]),
+              );
+            },
           );
-        });
+        } else if (state is NewestBooksFailure) {
+          return CustomErrorsWidget(errorMessage: state.errMessage);
+        } else {
+          return CustomLoadingIndicator();
+        }
+      },
+    );
   }
 }
