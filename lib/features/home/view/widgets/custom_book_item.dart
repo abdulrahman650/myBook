ddd1c22532bdcedb6e46a53bfb3f3ba551dcd3a6
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-01-03 22:00:45 +0200

    fixed error don't show pics in google

--- lib/features/home/view/widgets/custom_book_item.dart
+++ lib/features/home/view/widgets/custom_book_item.dart
@@ -2,76 +2,29 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/conestent/assets.dart';
-// class CustomBookImage extends StatelessWidget {
-//   const CustomBookImage({super.key, required this.imageUrl});
-//
-//   final String imageUrl;
-//   @override
-//   Widget build(BuildContext context) {
-//     return ClipRRect(
-//       borderRadius: BorderRadius.circular(16),
-//       child: AspectRatio(
-//         aspectRatio: 2.6 / 4,
-//         child: CachedNetworkImage(
-//           fit: BoxFit.fill,
-//           imageUrl: imageUrl,
-//           placeholder: (context, url) =>
-//           const Center(child: CircularProgressIndicator()),
-//           errorWidget: (context, url, error) => const Icon(
-//             Icons.error,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/material.dart';
-import '../../../../core/conestent/assets.dart';
-import '../../../../core/utiles/image_helper.dart';
-
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/material.dart';
-
-import '../../../../core/conestent/assets.dart';
-import '../../../../core/utiles/image_helper.dart';
-
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({
-    super.key,
-    required this.imageUrl,
-  });
+  const CustomBookImage({super.key, required this.imageUrl});
 
   final String imageUrl;
-
   @override
   Widget build(BuildContext context) {
-    final fixedUrl = ImageHelper.fixUrl(imageUrl);
-
     return ClipRRect(
       borderRadius: BorderRadius.circular(16),
       child: AspectRatio(
         aspectRatio: 2.6 / 4,
-        child: fixedUrl.isEmpty
-            ? Image.asset(Assets.imagesTestCover,fit: BoxFit.cover)
-            : CachedNetworkImage(
-          imageUrl: fixedUrl,
-          fit: BoxFit.cover,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
           placeholder: (context, url) =>
-          const Center(
-            child: SizedBox(
-              height: 24,
-              width: 24,
-              child: CircularProgressIndicator(strokeWidth: 2),
-            ),
+          const Center(child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
           ),
-          errorWidget: (context, url, error) =>
-              Image.asset(Assets.imagesTestCover, fit: BoxFit.cover),
         ),
       ),
     );
-
   }
 }
+
 
 
